50ec46ddba9a12e2f07eac0f32ef19122a47a6b8
Ginger Ben <ben.wilson@thevirtualforge.com>
2019-08-01 09:03:08 +0100

    remove page morph

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import styled from 'styled-components';
-import MorphTransition from 'nextjs-morph-page';
 import Menu from '../components/Menu';
 
 // Global data
-// import data from '../data.json';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -25,31 +23,7 @@ class MyApp extends App {
       <AppWrapper>
         <Container>
           <Menu />
-          {/* <MorphTransition timeout={300} classNames="morph"> */}
           <Component {...pageProps} />
-          {/* </MorphTransition> */}
-          <style jsx global>
-            {`
-              .morph.enter {
-                opacity: 0.01;
-                transform: scale(1.1);
-              }
-              .morph.enter.active {
-                opacity: 1;
-                transform: scale(1);
-                transition: all 300ms;
-              }
-              .morph.exit {
-                opacity: 1;
-                transform: scale(1);
-              }
-              .morph.exit.active {
-                opacity: 0.01;
-                transform: scale(1.1);
-                transition: all 300ms;
-              }
-            `}
-          </style>
         </Container>
       </AppWrapper>
     );
@@ -58,19 +32,4 @@ class MyApp extends App {
 
 export default MyApp;
 
-const AppWrapper = styled.div`
-  .page-transition-enter {
-    opacity: 0;
-  }
-  .page-transition-enter-active {
-    opacity: 1;
-    transition: opacity 500ms;
-  }
-  .page-transition-exit {
-    opacity: 1;
-  }
-  .page-transition-exit-active {
-    opacity: 0;
-    transition: opacity 500ms;
-  }
-`;
+const AppWrapper = styled.div``;
