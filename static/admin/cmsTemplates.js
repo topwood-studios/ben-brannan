cd5c7ed913e9daf5345d34a97d5b70c434146817
Ginger Ben <ben.wilson@thevirtualforge.com>
2019-09-03 21:15:35 +0100

    try importing templates

--- static/admin/cmsTemplates.js
+++ static/admin/cmsTemplates.js
@@ -1,5 +1,5 @@
-import CMS from "netlify-cms";
+// import CMS from "netlify-cms";
 
 import ProjectPreview from "./preview-templates/ProjectPreview";
 
-CMS.registerPreviewTemplate("projects", ProjectPreview);
+CMS.registerPreviewTemplate("projects", ProjectPreview); // eslint-disable-line
