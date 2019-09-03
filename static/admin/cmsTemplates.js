9ae6d0383b9c7478c0562373e43782e8b05697fb
Ginger Ben <ben.wilson@thevirtualforge.com>
2019-09-03 21:21:42 +0100

    I am completely lost at this stage.

--- static/admin/cmsTemplates.js
+++ static/admin/cmsTemplates.js
@@ -1,5 +1,5 @@
 // import CMS from "netlify-cms";
 
-import ProjectPreview from "./preview-templates/ProjectPreview";
+import ProjectPreview from "./preview-templates/ProjectPreview.js"; // eslint-disable-line
 
 CMS.registerPreviewTemplate("projects", ProjectPreview); // eslint-disable-line
