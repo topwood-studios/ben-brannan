f4fc31edf629f29611724f558e727e553571b9c7
Ginger Ben <ben@topwoodstudios.co.uk>
2019-10-20 10:15:58 +0100

    update meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import App, { Container } from 'next/app';
-import styled from 'styled-components';
-import Head from 'next/head';
+import React from "react";
+import App, { Container } from "next/app";
+import styled from "styled-components";
+import Head from "next/head";
 
 // Global data
-import content from '../content/settings/global.md';
+import content from "../content/settings/global.md";
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -20,7 +20,7 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     const {
-      attributes: { siteTitle, description },
+      attributes: { siteTitle, description, keywords }
     } = content;
 
     return (
@@ -28,6 +28,13 @@ class MyApp extends App {
         <Head>
           <title>{siteTitle}</title>
           <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+
+          {/* Facebook Open Graph tags */}
+          <meta property="og:title" content={siteTitle} />
+          <meta property="og:description" content={description} />
+          {/* TODO: Get Image */}
+          {/* <meta property="og:image" content={siteTitle} /> */}
         </Head>
         <Container>
           <Component {...pageProps} />
