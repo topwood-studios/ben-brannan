d333736d62ca1ebacaeed5c9984dc69ec3e2b6e0
Ginger Ben <ben@topwoodstudios.co.uk>
2019-11-20 19:53:20 +0000

    change transition duration

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -14,8 +14,8 @@ const Carousel = ({ slides }) => {
     enter: { opacity: 1 },
     leave: { opacity: 0 },
     config: {
-      tension: 500,
-      friction: 60,
+      tension: 280,
+      friction: 120,
       // duration: 250,
     },
   });
